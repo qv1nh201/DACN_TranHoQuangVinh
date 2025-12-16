bc84f7251121be20d36372a36a31f7a76e690ec7
Trần Hồ Quang Vinh <kocoten4192@gmail.com>
2025-12-16 23:45:45 +0700

    1

--- scripts/pub_sim.py
+++ scripts/pub_sim.py
@@ -1,7 +1,7 @@
 # pub_sim.py
 import requests, time, json
 
-API_URL = "http://localhost:8000/api/test_receive"  # dùng endpoint test_receive
+API_URL = "https://dacn-tranhoquangvinh.onrender.com"  # dùng endpoint test_receive
 
 for i in range(5):
     payload = {
